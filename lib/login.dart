cf1fa88d460af8a8e04725c79da9aa2080be3147
unknown <a278949@alumnos.uaslp.mx>
2024-09-18 21:29:00 -0600

    Login Agregadom, Segundo Commit

---
+++ lib/login.dart
@@ -0,0 +1,178 @@
+// login.dart
+import 'package:flutter/material.dart';
+import 'utils/constantes.dart';
+import 'dart:math';
+import 'home.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  String _errorMessage = '';
+
+  void _login(){
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    setState(() {
+      if(email.isEmpty && password.isEmpty){
+        _errorMessage = 'Datos Incompletos';
+      } else if (email != 'test@correo.mx' && password != 'FDM2') {
+        _errorMessage = 'Correo y contraseña incorrectos';
+      } else if (email != 'test@correo.mx') {
+        _errorMessage = 'Correo incorrecto';
+      } else if (password != 'FDM2') {
+        _errorMessage = 'Contraseña incorrecta';
+      } else {
+        _errorMessage = '';
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: fondo,
+      body: Stack(
+        children: [
+          // Cuadrados de colores al azar
+          Positioned.fill(
+            child: RandomColoredSquares(),
+          ),
+          Center(
+            child: Container(
+              width: 400.0,
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.5),
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Bienvenido a tu primer EXAMEN',
+                    style: TextStyle(color: titulos, fontSize: 24.0),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: const Text('Ingresa tu correo electronico:',
+                        style: TextStyle(
+                          color: Colors.white,
+                        )),
+                  ),
+                  TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Correo',
+                      border:OutlineInputBorder(
+                        borderRadius:BorderRadius.circular(20),
+                        borderSide: const BorderSide(
+                          width: 1,
+                          style: BorderStyle.none
+                        )
+                      )
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: const Text('Ingresa tu contraseña:',
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+                  ),
+                  TextField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Contraseña',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(
+                          width: 1,
+                          style: BorderStyle.none
+                        )
+                      )
+                    ),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 20.0),
+                  if(_errorMessage.isNotEmpty)
+                    Text(
+                      _errorMessage,
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  SizedBox(
+                    width: 300,
+                    child:
+                    ElevatedButton(
+                      onPressed: _login,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: botones,
+                      ),
+                      child: Text('Iniciar Sesión',
+                        style:
+                        TextStyle(color: Colors.white),),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    'Mi primer examen, ¿estará sencillo?',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RandomColoredSquares extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final random = Random();
+    final colors = [fondo, fondo2, fondo3, fondo4];
+    final int rows = 6;
+    final int columns = 4;
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: columns, // Número de columnas
+      ),
+      itemBuilder: (context, index) {
+        final row = index ~/ columns;
+        final column = index % columns;
+
+        // Generar una lista de colores aleatorios para cada fila
+        if (column == 0) {
+          colors.shuffle(random);
+        }
+
+        return Container(
+          decoration: BoxDecoration(
+            color: colors[column],
+            borderRadius: BorderRadius.circular(55.0),
+          ),
+        );
+      },
+      itemCount: rows * columns, // Número total de cuadrados
+    );
+  }
+}
---
+++ lib/utils/constantes.dart
@@ -0,0 +1,25 @@
+// constantes.dart
+import 'package:flutter/material.dart';
+
+const Color fondo = Color(0xff015236);
+const Color fondo2 = Color(0xff01644a);
+const Color fondo3 = Color(0xff01412e);
+const Color fondo4 = Color(0xff02b274);
+const titulos = Color(0xff96b602);
+const botones = Color(0xffdca900);
+const efectos = Color(0xff919b02);
+
+const List Lista = [
+  '3#15#12#Enero#María#López#Amantedelamúsica#7',
+  '4#22#8#Febrero#Juan#Pérez#Aficionadoalcine#9',
+  '5#30#19#Marzo#Ana#García#Entusiastadeldeporte#6',
+  '6#17#25#Abril#Carlos#Martínez#Fanáticodelatecnología#8',
+  '7#5#14#Mayo#Laura#Rodríguez#Apasionadaporlalectura#4',
+  '8#11#21#Junio#Pedro#Sánchez#Amantedelosanimales#3',
+  '9#28#9#Julio#Sofía#Fernández#Cocineraexperta#10',
+  '10#3#18#Agosto#Luis#Ramírez#Viajeroincansable#2',
+  '11#19#7#Septiembre#Elena#Torres#Fotógrafaaficionada#5',
+  '12#6#13#Octubre#Miguel#Gómez#Jardineroapasionado#1',
+  '13#24#22#Noviembre#Isabel#Díaz#Artistacreativa#6',
+  '14#10#30#Diciembre#Roberto#Morales#Amantedelcine#4',
+];
