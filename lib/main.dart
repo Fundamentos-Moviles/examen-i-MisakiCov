c8b622afb17cc2768fea56c436449b90f8acf0b8
Isaac Covarrubias <115163531+CovIsaac@users.noreply.github.com>
2024-09-19 14:02:27 -0600

    Add files via upload
    Home en proceso, tercer commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:examen1_cqi/home.dart';
 import 'package:flutter/material.dart';
 import 'package:examen1_cqi/login.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginScreen(),
+      home: HomeScreen(),
     );
   }
 }
